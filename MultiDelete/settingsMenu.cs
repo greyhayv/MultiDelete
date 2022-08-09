1ab4c7c297b96769ad0a950c33ef5bf1e731a9ed
greyhayv <greyhayv@gmail.com>
2022-08-09 12:23:22 +0200

    Removed more unused librarys

--- MultiDelete/settingsMenu.cs
+++ MultiDelete/settingsMenu.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using System.Threading.Tasks;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace MultiDelete
