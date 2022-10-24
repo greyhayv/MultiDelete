47f8bca79fa3b9393b83b3fba10b803abf91b3c0
greyhayv <greyhayv@gmail.com>
2022-10-24 16:00:06 +0200

    changed toolTip

--- MultiDelete/settingsMenu.cs
+++ MultiDelete/settingsMenu.cs
@@ -285,8 +285,8 @@ namespace MultiDelete
             toolTip.SetToolTip(addMultipleInstanceButton, "Add multiple Instances at once via selecting multiple folders");
             toolTip.SetToolTip(updateScreenLabel, "Select how often the screen should update during world deletion (Less updates = way faster world deletion)");
             toolTip.SetToolTip(updateScreenComboBox, "Select how often the screen should update during world deletion (Less updates = way faster world deletion)");
-            toolTip.SetToolTip(threadsToUseLabel, "Configure how many threads MultiDelte should use to delete worlds. More Threads = faster world deletion");
-            toolTip.SetToolTip(threadsTrackBar, "Configure how many threads MultiDelte should use to delete worlds. More Threads = faster world deletion");
+            toolTip.SetToolTip(threadsToUseLabel, "Configure how many threads MultiDelte should use to delete worlds.");
+            toolTip.SetToolTip(threadsTrackBar, "Configure how many threads MultiDelte should use to delete worlds.");
             toolTip.SetToolTip(keepLastWorldsLabel, "Select how many of the last worlds MultiDelete should keep.");
             toolTip.SetToolTip(keepLastWorldsNUD, "Select how many of the last worlds MultiDelete should keep.");
             toolTip.SetToolTip(keepLastWorldsLabel2, "Select how many of the last worlds MultiDelete should keep.");
