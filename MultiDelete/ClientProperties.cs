b6375ec4a2391fe5b76711bf19200d9d52a5d480
greyhayv <greyhayv@gmail.com>
2022-12-09 15:05:19 +0100

    Added empty clientproperties (this time fr)

---
+++ MultiDelete/ClientProperties.cs
@@ -0,0 +1,8 @@
+namespace MultiDelete
+{
+    public static partial class ClientProperties
+    {
+        public static readonly string CLIENT_ID = "";
+        public static readonly string CLIENT_SECRET = "";
+    }
+}
